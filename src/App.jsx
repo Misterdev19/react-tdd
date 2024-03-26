d62c458eca1300cb3f64b17c31e4a6b9ab84ae16
jcpenagos <juan_penagos@pana.co.com>
2024-03-26 18:01:03 -0500

    front end app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
+import logo from './assets/logo.svg'
 
 function App() {
 
 
   return (
-    <>
-     <h1>React TDD</h1>
-    </>
+    <main>
+      <aside>
+        <h1>Listado de elementos</h1>
+          <h2>Añadir o eliminar elementos</h2>
+        <form action="">
+          <label htmlFor="">
+              Elemento a introducir:
+            <input
+              type="text"
+              name='item'
+              required
+              placeholder='Agrega el nombre de un item' />
+          </label>
+          <button>Añadir elemento a la lista</button>
+        </form>
+      </aside>
+      <section>
+        <div className='list-elements'>
+          <h2>Elementos agregados</h2>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div> 
+        <div className='img-element'>
+           <img src={logo} alt="" />
+        </div>
+      </section>
+    </main>
   )
 }
 
